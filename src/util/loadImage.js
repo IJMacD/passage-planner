fbb5e7e5939b8e657373f858564c1f230ec1775b
Iain MacDonald <ijmacd@gmail.com>
2022-08-05 13:20:18 +0800

    Map updating works slightly better with a simple image cache

--- src/util/loadImage.js
+++ src/util/loadImage.js
@@ -1,13 +1,20 @@
+const cache = {};
+
 /**
  *
  * @param {string} src
  * @returns {Promise<HTMLImageElement>}
  */
 export function loadImage(src) {
+    if (cache[src]) {
+        return Promise.resolve(cache[src]);
+    }
+
     return new Promise((resolve, reject) => {
         const img = new Image();
         img.src = src;
         img.onload = () => resolve(img);
         img.onerror = () => reject(Error(`Unable to load ${src}`));
+        cache[src] = img;
     });
 }
