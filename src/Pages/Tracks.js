1da0233895b0c4dd0151908ce661506dd6757c85
Iain MacDonald <IJMacD@gmail.com>
2023-08-26 23:13:09 +0800

    More intuitive Tracks selection UI

--- src/Pages/Tracks.js
+++ src/Pages/Tracks.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { TrackDetails } from "../Components/TrackDetails.js";
 import { TrackEdit } from "../Components/TrackEdit.js";
 import { useAuthFetch } from "../hooks/useAuthFetch.js";
@@ -17,18 +17,23 @@ function Tracks () {
 
     const [ bgCheckboxes, setBgCheckboxes ] = useState(() => savedTracks.map(() => false));
 
-    const [ track, setTrack ] = useState(/** @type {Track?} */(null));
-
     const [ editMode, setEditMode ] = useState(false);
 
     const [ refreshToken, setRefreshToken ] = useSavedState('logbook.refreshToken', "");
+
+    const [ selectedTrackID, setSelectedTrackID ] = useState(-1);
 
     const authFetch = useAuthFetch({
         exchangeURL: "https://passage.ijmacd.com/logbook/api/v1/auth/exchange",
         refreshToken,
     });
 
-    function loadTrack (trackSerialized) {
+    const track = useMemo(() => {
+        if (selectedTrackID < 0) {
+            return null;
+        }
+
+        const trackSerialized = savedTracks[selectedTrackID];
         const segments = trackSerialized.segments.map(seg => {
             return seg.map(point => {
                 if (typeof point.time === "string") {
@@ -37,11 +42,16 @@ function Tracks () {
                 return point;
             });
         });
-        setTrack({ ...trackSerialized, segments });
-    }
 
+        return { ...trackSerialized, segments };
+    }, [selectedTrackID]);
+
+    /**
+     * @param {number} index
+     */
     function removeTrack (index) {
         setSavedTracks(tracks => [...tracks.slice(0, index), ...tracks.slice(index+1) ]);
+        setSelectedTrackID(oldID => oldID === index ? -1 : oldID);
     }
 
     /**
@@ -62,9 +72,9 @@ function Tracks () {
 
         const body = new FormData();
         body.set("total_distance", totalDistance.toFixed(3));
-        body.set("start_location", "Peng Chau, HK");
+        body.set("start_location", "Hong Kong");
         body.set("start_time", startTime.toISOString());
-        body.set("end_location", "Peng Chau, HK");
+        body.set("end_location", "Hong Kong");
         body.set("end_time", endTime.toISOString());
         body.set("weather", "");
         body.set("comments", "");
@@ -113,7 +123,7 @@ function Tracks () {
                     if (gpxDoc.tracks.length > 0) {
                         const track = gpxDoc.tracks[0];
                         setSavedTracks(tracks => [ ...tracks, track ]);
-                        setTrack(track);
+                        setSelectedTrackID(savedTracks.length - 1);
                     }
                 }
             });
@@ -175,22 +185,22 @@ function Tracks () {
 
             <div>
                 <div>
-                    <ul>
+                    <ul style={{listStyle:"none",padding:0}}>
                         {
                             savedTracks.map((t, i) => (
                                 <li key={i}>
-                                    <input type="checkbox" checked={bgCheckboxes[i]||false} onChange={e => toggleBgCheckbox(i, e.target.checked)} />{' '}
+                                    <input type="radio" name="track" checked={selectedTrackID === i} onChange={() => setSelectedTrackID(i)} />{' '}
+                                    <input type="checkbox" checked={bgCheckboxes[i]||false} disabled={selectedTrackID === i} onChange={e => toggleBgCheckbox(i, e.target.checked)} />{' '}
                                     {t.name}{' '}
-                                    <button onClick={() => loadTrack(t)}>Load</button>{' '}
                                     <button onClick={() => uploadTrack(t)} disabled={!refreshToken}>Upload</button>{' '}
                                     <button onClick={() => removeTrack(i)}>Remove</button>{' '}
                                 </li>)
                             )
                         }
                     </ul>
-                    <button onClick={() => setTrack(null)}>Clear</button>
-                    <button onClick={() => setEditMode(!editMode)}>{editMode?"View":"Edit"}</button>
-                    <button onClick={handleDownload}>Download</button>
+                    <button onClick={() => setSelectedTrackID(-1)} disabled={selectedTrackID<0}>Clear</button>
+                    <button onClick={() => setEditMode(!editMode)} disabled={selectedTrackID<0}>{editMode?"View":"Edit"}</button>
+                    <button onClick={handleDownload} disabled={selectedTrackID<0}>Download</button>
                     {!refreshToken && <button onClick={() => handleLogin()}>Login</button>}
                     <br/>
                     <input type="file" onChange={handleFileLoad} />
