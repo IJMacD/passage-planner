97c78d7226694253c25c7867d87a3bd46a15d332
Iain MacDonald <IJMacD@gmail.com>
2023-10-11 14:24:00 +0800

    Gradient doesn't *have* to use worker

--- src/Layers/GradientFieldWorkerLayer.jsx
+++ src/Layers/GradientFieldWorkerLayer.jsx
@@ -1,25 +1,18 @@
 import { useContext, useEffect, useRef } from "react";
 import { DragContext, StaticMapContext } from "../Components/StaticMap.jsx";
 import React from "react";
-import { calculateGradient } from "../util/calculateGradient.js";
+import gradientFieldWorker from '../workers/gradientFieldWorker.js?worker'
 
 /**
  *
  * @param {object} props
- * @param {import("./VectorFieldLayer.js").Field} props.field
+ * @param {import("./VectorFieldLayer.jsx").Field} props.field
  * @param {number} [props.alpha]
  * @param {number} [props.rangeLimit] In nautical miles
  * @param {number} [props.scale] Colour scale mapping: from min to max wind => 0 to 360 deg
- * @param {HTMLCanvasElement|null|undefined} [props.mask]
  * @returns {React.JSX.Element}
  */
-export function GradientFieldLayer ({
-    field,
-    alpha = 255,
-    rangeLimit = 10,
-    scale = 2.5,
-    mask = undefined,
-}) {
+export function GradientFieldLayer ({ field, alpha = 255, rangeLimit = 10, scale = 2.5 }) {
     const context = useContext(StaticMapContext);
 
     const [left,top] = useContext(DragContext);
@@ -29,32 +22,30 @@ export function GradientFieldLayer ({
 
     const { width, height } = useContext(StaticMapContext);
 
+    const workerRef = useRef(/** @type {Worker?} */(null));
+
     const pxWidth = width * devicePixelRatio;
     const pxHeight = height * devicePixelRatio;
 
     useEffect(() => {
         if (!canvasRef.current) return;
 
-        canvasRef.current.width = pxWidth;
-        canvasRef.current.height = pxHeight;
+        if (!workerRef.current) {
+            const offscreenCanvas = canvasRef.current.transferControlToOffscreen();
 
-        if (mask === null) {
-            // mask !== undefined
-            // mask is specified but not ready
-            return;
+            const worker = new gradientFieldWorker();
+
+            worker.postMessage({canvas: offscreenCanvas}, [offscreenCanvas]);
+
+            workerRef.current = worker;
         }
+    }, []);
 
-        const ctx = canvasRef.current.getContext("2d");
+    useEffect(() => {
+        if (!workerRef.current) return;
 
-        if (ctx) {
-            calculateGradient(ctx, context, pxWidth, pxHeight, field, alpha, rangeLimit, scale);
-
-            if (mask) {
-                ctx.globalCompositeOperation = "destination-in";
-                ctx.drawImage(mask, 0, 0);
-            }
-        }
-    }, [context, pxWidth, pxHeight, field, alpha, rangeLimit, scale, mask]);
+        workerRef.current.postMessage({ context, pxWidth, pxHeight, field, alpha, rangeLimit, scale });
+    }, [context, pxWidth, pxHeight, field, alpha, rangeLimit, scale]);
 
     return <canvas ref={canvasRef} width={pxWidth} height={pxHeight} style={{ width: "100%", height: "100%", position: "absolute", top, left }} />;
 }
