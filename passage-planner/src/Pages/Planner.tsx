e2e64df12a492e027ba980a7fdcee76b86026d6b
Iain MacDonald <iain@smartretail.co>
2024-11-07 15:48:07 +0800

    WIP

--- passage-planner/src/Pages/Planner.tsx
+++ passage-planner/src/Pages/Planner.tsx
@@ -2,13 +2,15 @@ import { useState, CSSProperties } from "react";
 import { StaticMap } from "../Components/StaticMap";
 import { HongKongMarineLayer } from "../Layers/HongKongMarineLayer";
 
+type Longitude = number;
+type Latitude = number;
+type Point = [Longitude, Latitude];
 type ISOLocalDateString = string;
 
 const ONE_HOUR = 60 * 60 * 1000;
 
 export function Planner() {
-  const [centre, setCentre] = useState([114.2, 22.3] as [number, number]);
-  const [zoom, setZoom] = useState(14);
+  const [locations, setLocations] = useState([{ index: 0, centre: [114.2, 22.3] as Point, zoom: 14 }]);
   const [panelCount, setPanelCount] = useState(3);
   const [startTime, setStartTime] = useState(() => isoLocalDateTime());
   const [panelDelta, setPanelDelta] = useState(3 * ONE_HOUR);
@@ -39,6 +41,10 @@ export function Planner() {
     fontWeight: "bold",
   };
 
+  const pinnedLocationStyle: CSSProperties = {
+    cursor: "pointer",
+  };
+
   const timeFormatter = Intl.DateTimeFormat([], { timeStyle: "medium" });
 
   return (
@@ -57,18 +63,64 @@ export function Planner() {
       </aside>
       <main style={mainStyle}>
         {Array.from({ length: panelCount }).map((_, i) => {
+          const { centre, zoom, index } = findLast(locations, l => l.index <= i);
           const time = new Date(+new Date(startTime) + i * panelDelta);
+
+          function handleDragEnd(lon: Longitude, lat: Latitude) {
+            setLocations(locations => {
+              const before = locations.filter(l => l.index < i);
+              const after = locations.filter(l => l.index > i);
+
+              return [
+                ...before,
+                { index: i, centre: [lon, lat], zoom },
+                ...after
+              ];
+            });
+          }
+
+          function handleDoubleClick(lon: Longitude, lat: Latitude, e: React.MouseEvent) {
+            setLocations(locations => {
+              const before = locations.filter(l => l.index < i);
+              const after = locations.filter(l => l.index > i);
+
+              return [
+                ...before,
+                { index: i, centre, zoom: (e.ctrlKey || e.metaKey) ? zoom - 1 : zoom + 1 },
+                ...after
+              ];
+            });
+          }
+
+          function handlePinnedClick() {
+            if (i === 0) {
+              // We can never remove the first point
+              return;
+            }
+
+            setLocations(locations => {
+              const before = locations.filter(l => l.index < i);
+              const after = locations.filter(l => l.index > i);
+
+              return [
+                ...before,
+                ...after
+              ];
+            });
+
+          }
 
           return (
             <section style={panelStyle} key={i}>
               <span style={timeStyle}>{timeFormatter.format(time)}</span>
+              {index === i && <span style={pinnedLocationStyle} onClick={handlePinnedClick}>•</span>}
               <StaticMap
                 centre={centre}
                 zoom={zoom}
                 width={400}
                 height={300}
-                onDragEnd={(lon, lat) => setCentre([lon, lat])}
-                onDoubleClick={(lat, lon, e) => setZoom(z => e.ctrlKey ? z - 1 : z + 1)}
+                onDragEnd={handleDragEnd}
+                onDoubleClick={handleDoubleClick}
               >
                 <HongKongMarineLayer />
               </StaticMap>
@@ -92,4 +144,14 @@ function isoLocalDateTime(date = new Date()): ISOLocalDateString {
     }:${p2(date.getMinutes())
     }:${p2(date.getSeconds())
     }`;
+}
+
+function findLast<T>(array: T[], callback: (element: T) => boolean) {
+  for (let i = array.length - 1; i >= 0; i--) {
+    if (callback(array[i])) {
+      return array[i];
+    }
+  }
+
+  return undefined;
 }
