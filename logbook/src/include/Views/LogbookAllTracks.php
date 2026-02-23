77df3bab54baeee0f677ccb63615cd1adaed6a6e
Iain MacDonald <IJMacD@gmail.com>
2026-02-23 21:05:54 +0800

    Ability to filter "all" tracks by date spec

--- logbook/src/include/Views/LogbookAllTracks.php
+++ logbook/src/include/Views/LogbookAllTracks.php
@@ -1,7 +1,11 @@
 <?php
 
 $title = "Logbook Tracks";
-$tracks = getAllEntries($db);
+$params = [];
+if (isset($_GET['date'])) {
+    $params['dateSpec'] = $_GET['date'];
+}
+$tracks = getAllEntries($db, $params);
 $content = include_contents("Templates/all_tracks.php", [
     "title" => $title,
     "tracks" => $tracks
